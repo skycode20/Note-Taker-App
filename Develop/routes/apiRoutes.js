13e2419192e14cf3ca1121760bd1f6820f65424b
Skyler R <skyler.rencher@gmail.com>
2020-11-01 21:02:57 -0500

    added the post and delete methods to the notes.html link. added the module.exports.

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const store = require("../db/store");
 
 router.get("/notes", (req, res) => {
     store
@@ -6,3 +7,20 @@ router.get("/notes", (req, res) => {
         .then((notes) => res.json(notes))
         .catch((err) => res.status(500).json(err));
 });
+
+router.post("/notes", (req, res) => {
+    store
+        .addNote(req.body)
+        .then((note) => res.json(note))
+        .catch((err) => res.status(500).json(err));
+});
+
+// DELETE "/api/notes" deletes the note with an id equal to req.params.id
+router.delete("/notes/:id", (req, res) => {
+    store
+        .removeNote(req.params.id)
+        .then(() => res.json({ ok: true }))
+        .catch((err) => res.status(500).json(err));
+});
+
+module.exports = router;
