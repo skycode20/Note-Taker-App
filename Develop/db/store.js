4ed7fb4e36036111dd29b85d6934c876a7ba27af
Skyler R <skyler.rencher@gmail.com>
2020-11-01 21:38:13 -0500

    created a new class called Store. Put read, write, and get note constructor functions inside of it. exported the new Store();.

--- Develop/db/store.js
+++ Develop/db/store.js
@@ -5,3 +5,31 @@ const uuidv1 = require("uuid/v1");
 
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
+
+class Store {
+    read() {
+        return readFileAsync("db/db.json", "utf8");
+    }
+
+    write(note) {
+        return writeFileAsync("db/db.json", JSON.stringify(note));
+    }
+
+    getNotes() {
+        return this.read().then((notes) => {
+            let parsedNotes;
+
+            // If notes isn't an array or can't be turned into one, send back a new empty array
+            try {
+                parsedNotes = [].concat(JSON.parse(notes));
+            } catch (err) {
+                parsedNotes = [];
+            }
+
+            return parsedNotes;
+        });
+    }
+
+}
+
+module.exports = new Store();
