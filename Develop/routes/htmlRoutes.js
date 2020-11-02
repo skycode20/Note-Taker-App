6642c97818f2ecf9d69cb3695858f6fbcab919c1
Skyler R <skyler.rencher@gmail.com>
2020-11-01 20:47:41 -0500

    added the path and router requirements to the htmlRoutes.js file. Linked the notes.html file.

---
+++ Develop/routes/htmlRoutes.js
@@ -0,0 +1,8 @@
+const path = require("path");
+const router = require("express").Router();
+
+// "/notes" responds with the notes.html file
+router.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/notes.html"));
+});
+
