0512399e27ea2aab3c344cd70d5730c78308d2b3
Skyler R <skyler.rencher@gmail.com>
2020-11-01 20:50:22 -0500

    added the link to the index.html file and exported router.

--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -1,8 +1,13 @@
 const path = require("path");
 const router = require("express").Router();
 
-// "/notes" responds with the notes.html file
+
 router.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/notes.html"));
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
 
+router.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+module.exports = router;
