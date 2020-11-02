4e79b06f0d6b07b6f3a49e50efc4d73852e6e487
Skyler R <skyler.rencher@gmail.com>
2020-11-01 21:44:08 -0500

    added the addNote and removeNote constructor functions within the store Class.

--- Develop/db/store.js
+++ Develop/db/store.js
@@ -30,6 +30,30 @@ class Store {
         });
     }
 
+    addNote(note) {
+        const { title, text } = note;
+
+        if (!title || !text) {
+            throw new Error("Note 'title' and 'text' cannot be blank");
+        }
+
+        // Add a unique id to the note using uuid package
+        const newNote = { title, text, id: uuidv1() };
+
+        // Get all notes, add the new note, write all the updated notes, return the newNote
+        return this.getNotes()
+            .then((notes) => [...notes, newNote])
+            .then((updatedNotes) => this.write(updatedNotes))
+            .then(() => newNote);
+    }
+
+    removeNote(id) {
+        // Get all notes, remove the note with the given id, write the filtered notes
+        return this.getNotes()
+            .then((notes) => notes.filter((note) => note.id !== id))
+            .then((filteredNotes) => this.write(filteredNotes));
+    }
+
 }
 
 module.exports = new Store();
